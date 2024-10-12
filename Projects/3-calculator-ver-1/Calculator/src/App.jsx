bdee0a2523e87d0d58876d6d003c0ca2478a6cd4
sujaldhakal369 <suzaldhakal1@gmail.com>
2024-10-12 20:42:51 +0545

    day 10

--- Projects/3-calculator-ver-1/Calculator/src/App.jsx
+++ Projects/3-calculator-ver-1/Calculator/src/App.jsx
@@ -5,7 +5,7 @@ import BtnContainer from "./components/BtnContainer";
 function App() {
   return (
     <>
-      <h1>Calculator</h1>
+      <h1>Calculator App</h1>
       <div className={styles.calculator}>
         <Display></Display>
         <BtnContainer></BtnContainer>
