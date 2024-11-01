ae890912e85660ddfab76bd10bebe9482528247d
sujaldhakal369 <suzaldhakal1@gmail.com>
2024-11-01 20:47:52 +0545

    day 25

--- Projects/1-todo-app-version-3/src/components/WelcomeMessge.jsx
+++ Projects/1-todo-app-version-3/src/components/WelcomeMessge.jsx
@@ -3,7 +3,7 @@ import { TodoItemsContext } from "../store/todo-items-store";
 
 const WelcomeMessage = () => {
   const { todoItems } = useContext(TodoItemsContext);
-  return todoItems.length === 0 && <p>No task to do.</p>;
+  return todoItems.length === 0 && <p>you have no task to do.</p>;
 };
 
 export default WelcomeMessage;
