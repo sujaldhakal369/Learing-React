2d320c52137ee9eb7ca32dcde202ffa777e96970
sujaldhakal369 <suzaldhakal1@gmail.com>
2024-11-10 19:31:17 +0545

    day 27

---
+++ Projects/4-social-media/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Slidebar from "./components/Slidebar";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Slidebar></Slidebar>
+      <div className="content">
+        <Header></Header>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
